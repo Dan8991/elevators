36ff578cad7f42e73addf6f30b41ce5faf9310a1
daniele <daniseas98@gmail.com>
2019-05-31 10:31:20 +0200

    [Change]completed basic elevator.h

--- elevator.h
+++ elevator.h
@@ -2,16 +2,22 @@
 #define ELEVATOR_H
 
 #include "pbl/src/src/pbl.h"
-#include "persona.h"
+#include "person.h"
+
+#define MAX_ELEVATOR_CAPACITY 12
 
 typedef struct elevator{
     person_t *in_people; 
+    int in_people_number;
     person_t *wait_people;
     int current_floor;
+    int destination;
 }elevator_t;
 
 elevator_t get_elevator();
 
-void forward_time();
+void forward_time(elevator_t *elevator);
+
+void choose_next_destination(elevator_t *elevator);
 
 #endif
