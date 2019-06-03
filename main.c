84aaec9846c8186e979993465f210f960a9b1a2b
daniele <daniseas98@gmail.com>
2019-06-03 19:15:12 +0200

    [Change]improved syntax and added comments

--- main.c
+++ main.c
@@ -8,31 +8,54 @@
 
 int main(int argv, char **argc){
 
+	//input file where the commands can be found
     FILE *input_file = fopen(argc[1], "r");
+
+	//temp string that will contain the command found in one of the lines in the file
     char command[MAX_INPUT_LINE_LENGTH]; 
+
+	//system time
     int current_time = 0;
+
+	//array of queues for each floor
     queue_t floors[MAX_FLOOR];
+
+	//array of elevators in the hotel
     elevator_t elevators[2];
+
+	//initializing elevators and floors queues
     elevators[0] = get_elevator();
     elevators[1] = get_elevator();
     initialize_floors(floors);
 
 	//TODO second elevator follows first until it is full
 
-    while(fgets(command, MAX_INPUT_LINE_LENGTH, input_file)){
+    while(fgets(command, MAX_INPUT_LINE_LENGTH, input_file))
+	{
+		//temp string containing the line
         char base_command[MAX_INPUT_LINE_LENGTH];
+
+		//extracting the command alone from the line
         sscanf(command, "%s", base_command);
-        if(strcmp(base_command, PRINT_STATUS) == 0){
+
+		//execute a different function for each of the three different commands
+        if(strcmp(base_command, PRINT_STATUS) == 0)
+		{
             print_status(elevators);
-        } else if(strcmp(base_command, "TIME_STEP") == 0){
+        } 
+		else if(strcmp(base_command, "TIME_STEP") == 0)
+		{
             current_time++;
             time_step(elevators, floors, current_time);
-        } else {
+        } 
+		else
+		{
             person_arrives(command, floors, current_time);
         }
 		
     }
 
+	//freeing memory
     fclose(input_file);
     free_elevator(elevators + 1);
     free_elevator(elevators);
