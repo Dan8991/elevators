3341479ffbe6180fc9b90885d0791065b6d5e9fd
daniele <daniseas98@gmail.com>
2019-06-03 17:04:14 +0200

    [Change]improved code readability

--- node.c
+++ node.c
@@ -1,22 +1,25 @@
 #include <stdlib.h>
 #include "node.h"
 
-node_t *create_node(void *value){
-
+node_t *create_node(void *value)
+{
     node_t *ret = malloc(sizeof(node_t));
 
     ret->value = value;
     ret->next = NULL; 
     ret->previous = NULL;
+
     return ret;
 }
 
-node_t *free_node(node_t *node, void (*free_value)(void*)){
-
-    if(free_value){
+node_t *free_node(node_t *node, void free_value(void*))
+{
+    if(free_value)
+	{
         free_value(node->value);
     }
 
     free(node);
+
     return NULL;
 }
--- node.h
+++ node.h
@@ -1,15 +1,31 @@
 #ifndef LIST_H
 #define LIST_H
 
-
-typedef struct node{
+/*
+node of a linked_list, contains a reference to the previous and next nodes
+and also a generic pointer to a value
+*/
+typedef struct node
+{
     void *value;
     struct node *previous;
     struct node *next;
-} node_t;
+}
+node_t;
 
+/*
+*constructor of a node, memory is allocated in the heap so it needs to be freed
+* value = pointer to the value to be incapsulated in the node
+* returns a pointer to the newly created node
+*/
 node_t *create_node(void *value);
 
-node_t *free_node(node_t *node, void (*free_value)(void*));
+/*
+*Function used to free a node and it's value
+* node = node to be freed
+* free_value = function that frees the memory of the value contained in the node
+* returns NULL
+*/
+node_t *free_node(node_t *node, void free_value(void*));
 
 #endif
