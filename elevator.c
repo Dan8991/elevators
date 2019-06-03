b344a537d3a59732eff7f60f36dd44f6942e52b0
daniele <daniseas98@gmail.com>
2019-06-03 12:36:26 +0200

    [Patch]removed useless field and fixed a bug with choose_prioirty

--- elevator.c
+++ elevator.c
@@ -11,7 +11,6 @@ elevator_t get_elevator(){
         in_people: create_list(),
         current_floor: 0,
         destination: 0,
-        total_time: 0
     };
 }
 
@@ -24,7 +23,6 @@ void forward_time(elevator_t *elevator, queue_t *people_queue, int total_time){
     } else if(elevator->current_floor < elevator->destination) {
         elevator->current_floor++;
     }
-    elevator->total_time++;
 }
 
 
@@ -36,6 +34,7 @@ elevator_t *free_elevator(elevator_t *elevator){
 
 void choose_next_destination(elevator_t *elevator, queue_t *floors, int total_time){
     int priorities[MAX_FLOOR] = {0};
+	reset_iterator(elevator->in_people);
     while(iter_has_next(elevator->in_people)){
         person_t *person = (person_t*) move_next(elevator->in_people);
         priorities[person->destination] += total_time - person->arrival_time;
@@ -64,21 +63,22 @@ void choose_next_destination(elevator_t *elevator, queue_t *floors, int total_ti
     elevator->destination = max_floor;
 }
 
-void enter_people(elevator_t *elevator, queue_t *people_queue){
+void enter_people(elevator_t *elevator, queue_t *people_queue, int total_time){
     while(elevator_load(elevator) < MAX_ELEVATOR_CAPACITY && 
             !queue_is_empty(people_queue )){
         person_t *person = dequeue_element(people_queue);
-        person->arrival_time = elevator->total_time;
+        person->arrival_time = total_time;
         add(elevator->in_people, person);
         people_queue->start_time = elevator->total_time;
     }
 }
 
 void exit_people(elevator_t *elevator){
+	reset_iterator(elevator->in_people);
     while(iter_has_next(elevator->in_people)){
         person_t *person = move_next(elevator->in_people);
         if(person->destination == elevator->current_floor){
-            remove_current_iter_node(elevator->in_people, free);
+        	remove_current_iter_node(elevator->in_people, free);
         }
     }    
 }
