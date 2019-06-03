8fe324e2e6b6714479ece0cf2f517c9cdc9ab332
daniele <daniseas98@gmail.com>
2019-06-03 19:08:19 +0200

    [Change]improved code readability and added comments

--- hotel.c
+++ hotel.c
@@ -5,38 +5,67 @@
 #include "queue.h"
 #include "elevator.h"
 
-void person_arrives(char *string, queue_t *floors, int current_time){
+void person_arrives(char *command, queue_t *floors, int current_time)
+{
     int arrival;
     int destination;
+
+	//useless string used just to contain the string in the command
     char mock[MAX_INPUT_LINE_LENGTH];
-    sscanf(string, "%s %d %d", mock, &arrival, &destination);
-    queue_element(floors + arrival, get_person(destination, arrival, current_time), current_time);
+
+	//reading arrival and destination
+    sscanf(command, "%s %d %d", mock, &arrival, &destination);
+
+	//queueing the person at the proper floor's queue
+    queue_element(
+		floors + arrival,
+		get_person(destination, arrival, current_time),
+		current_time
+	);
 }
 
-void time_step_elevator(elevator_t *elevator,queue_t *floors, int total_time){
+void time_step_elevator(elevator_t *elevator,queue_t *floors, int total_time)
+{
+	//first of all the people that reached the correct destination exit the elevator
 	exit_people(elevator);
-    if(!queue_is_empty(floors + elevator->current_floor)){
+
+	/*
+	if the queue at the current floor is not empty anyone who
+	can enters the elevator
+	*/
+    if(!queue_is_empty(floors + elevator->current_floor))
+	{
         enter_people(elevator, floors + elevator->current_floor, total_time);
     }
 }
 
-void time_step(elevator_t *elevators, queue_t *floors, int total_time){
-    time_step_elevator(elevators, floors, total_time);
-    time_step_elevator(elevators + 1, floors, total_time);
+void time_step(elevator_t *elevators, queue_t *floors, int total_time)
+{
+	//the elevators moove 
     forward_time(elevators, floors, total_time);
     forward_time(elevators + 1, floors, total_time);
+
+	//the elevators let the people in and out at the current floor
+    time_step_elevator(elevators, floors, total_time);
+    time_step_elevator(elevators + 1, floors, total_time);
 }
 
-void print_status(elevator_t *elevators){
+void print_status(elevator_t *elevators)
+{
     printf("PRINT_STATUS:\n\n");
+
+	//temp string that will contain the elevators infos
 	char temp[MAX_ELEVATOR_STRING_LENGTH];
+
 	printf("%s", elevator_to_string(temp, elevators));
 	printf("%s", elevator_to_string(temp, elevators + 1));
 }
 
 
-void initialize_floors(queue_t *floors){
-    for(int i = 0; i < MAX_FLOOR; i++){
+void initialize_floors(queue_t *floors)
+{
+    for(int i = 0; i < MAX_FLOOR; i++)
+	{
         floors[i] = get_queue();
     }
 }
--- hotel.h
+++ hotel.h
@@ -5,14 +5,36 @@
 
 #define PRINT_STATUS "PRINT_STATUS" 
 #define TIME_STEP "TIME_STEP" 
+
+//max length of a single command
 #define MAX_INPUT_LINE_LENGTH 23
 
-void person_arrives(char *string, queue_t *floors, int current_time);
+/*
+*Function that handles the arrival of a new person
+* command = command from the input file
+* floors = array of the queues for every floors
+* current_time = time passed since the beginning
+*/
+void person_arrives(char *command, queue_t *floors, int current_time);
 
-void time_step(elevator_t *elevators, queue_t *floors, int time);
+/*
+*Function that handles a time_step
+* elevators = array of the elevators in the hotel
+* floors = array of queues at the various floors
+* time_passed = time passed since the beginning
+*/
+void time_step(elevator_t *elevators, queue_t *floors, int time_passed);
 
+/*
+*function that prints the status of the hotel
+* elevators = array that contains the two elevators
+*/
 void print_status(elevator_t *elvators);
 
+/*
+*Function that initializes the queues for each floor
+* floors = array of queues
+*/
 void initialize_floors(queue_t *floors);
 
 #endif
